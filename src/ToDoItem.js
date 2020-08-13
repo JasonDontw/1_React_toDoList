c9571eaf20e24eebf4ab9f2719aaaa9168eba228
Jason Dong <ee13751@gmail.com>
2020-08-13 22:13:23 +0800

    新增生命週期函數用法

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,4 +1,5 @@
 import React,{ Component, Fragment} from 'react';
+import propTypes from 'prop-types'
 
 class ToDoItem extends Component{
 
@@ -10,21 +11,35 @@ class ToDoItem extends Component{
   render(){
     return (
       <Fragment>
-        {/* <li onClick={this.handleClick}> */}
+        {/* <li onClick={this.handleClick}> */} {/*第2種方法*/}
         <li onClick={()=> this.props.handleDelete(this.props.index)}> {/*子組件要使用父組件的函式時必須由子組件的函示包裹才能使用*/}
           {this.props.item}
         </li>
       </Fragment>
+
     );
   }
 
-  
-  handleClick(){   {/*第2種方法*/}
-    this.props.handleDelete(this.props.index);
+  shouldComponentUpdate(nextProps, nextState){ //讓子組件被創建後不會再因為父組件的更動而從新渲染，用來提升效能
+      if(nextProps.item !== this.props.item){ //判斷下一個傳入的item及現有的一不一樣，因為shouldComponentUpdate會在渲染前執行，所以
+        return true;                          //this.props.item還是舊的資料
+      }else{
+        return false;
+      }
   }
 
+  handleClick(){   //第2種方法
+    this.props.handleDelete(this.props.index);
+  }
 }
 
+ToDoItem.propTypes = {  //限制父組件傳入值的型別，不強制限制傳錯會報錯
+  item: propTypes.oneOfType([propTypes.string,propTypes.number]), //輸入的值是其中一種型別
+  handleDelete: propTypes.func,
+  index: propTypes.number
+}
 
-
+ToDoItem.defaultProps = {  //定義如果沒有傳遞值進來的話給的默認值
+  item: 'HI'
+}
 export default ToDoItem;
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,6 +1,7 @@
 import React,{ Component, Fragment} from 'react';
 import ToDoItem from './ToDoItem';
 import './style.css';
+import axios from 'axios';
 
 class ToDoList extends Component{
 
@@ -25,22 +26,34 @@ class ToDoList extends Component{
             className='input'
             value={this.state.inputValue} type="text"
             onChange={this.handleChange}
+            ref={(input) => { this.input_value = input}} //(數據傳入寫法二)(盡量避免使用ref來獲取數值)
           />
           <button type="button" onClick={this.handleSubmit}>提交</button>
         </div>
 
-        <ul>
+        <ul  ref={(ul) => { this.ul_value = ul}}>
           {
             this.state.list.map((item , index) =>{
               return (
                   <ToDoItem key={index} handleDelete={this.handleDelete} item={item} index={index}/>
-
               )
             })
           }
         </ul>
       </Fragment>
     );
+  }
+
+  componentDidMount(){ //用於放AJAX事先載入資料時  下方為測試用
+  //  axios.get('https://images.unsplash.com/photo-1516550135131-fe3dcb0bedc7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=621e8231a4e714c2e85f5acbbcc6a730&auto=format&fit=crop&w=1352&q=80').then(
+  //   (res)=>{ 
+  //     alert('true');
+  //   }
+  //  ).catch(
+  //   ()=>{
+  //     alert('false');
+  //   }
+  //  )
   }
 
   handleChange(e){
@@ -50,7 +63,9 @@ class ToDoList extends Component{
     //   }
     // })
     
-    const value = e.target.value    //因為函式寫法為異步執行所以必須先把值保存起來
+    const value = e.target.value    //因為函式寫法為異步執行所以必須先把值保存起來 (數據傳入寫法一)
+    const value2 = this.input_value.value //(數據傳入寫法二)
+
     this.setState(()=>({            //Return 簡寫寫法
       inputValue : value
     }))
@@ -62,7 +77,11 @@ class ToDoList extends Component{
     this.setState((prevState)=>({  //prevState是紀錄觸發此函術前的State狀態
       list : [...prevState.list , prevState.inputValue],
       inputValue : ''
-    }))
+    }),()=>{ 
+        console.log(this.ul_value.querySelectorAll('li').length+'內部'); //setState的內建的Callback函數，setState執行完成後執行，避免異步執行的錯誤
+    });
+
+        console.log(this.ul_value.querySelectorAll('li').length+'外部'); //因為setState是異步執行所以不會馬上渲染，導致執行時只會撈到舊的
   }
 
   handleDelete(index){
